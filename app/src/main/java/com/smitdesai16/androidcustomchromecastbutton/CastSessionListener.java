77f3ad47ba13e1e729cab4eace588b27bcb6f227
Smit Desai <smit.desai16@gmail.com>
2021-04-23 13:38:31 -0500

    Adding cast custom button

---
+++ app/src/main/java/com/smitdesai16/androidcustomchromecastbutton/CastSessionListener.java
@@ -0,0 +1,51 @@
+package com.smitdesai16.androidcustomchromecastbutton;
+
+import com.google.android.gms.cast.framework.Session;
+import com.google.android.gms.cast.framework.SessionManagerListener;
+
+public class CastSessionListener implements SessionManagerListener {
+    @Override
+    public void onSessionStarting(Session session) {
+
+    }
+
+    @Override
+    public void onSessionStarted(Session session, String s) {
+
+    }
+
+    @Override
+    public void onSessionStartFailed(Session session, int i) {
+
+    }
+
+    @Override
+    public void onSessionEnding(Session session) {
+
+    }
+
+    @Override
+    public void onSessionEnded(Session session, int i) {
+
+    }
+
+    @Override
+    public void onSessionResuming(Session session, String s) {
+
+    }
+
+    @Override
+    public void onSessionResumed(Session session, boolean b) {
+
+    }
+
+    @Override
+    public void onSessionResumeFailed(Session session, int i) {
+
+    }
+
+    @Override
+    public void onSessionSuspended(Session session, int i) {
+
+    }
+}
--- app/src/main/java/com/smitdesai16/androidcustomchromecastbutton/MainActivity.java
+++ app/src/main/java/com/smitdesai16/androidcustomchromecastbutton/MainActivity.java
@@ -1,14 +1,40 @@
 package com.smitdesai16.androidcustomchromecastbutton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.mediarouter.app.MediaRouteButton;
 
 import android.os.Bundle;
 
+import com.google.android.gms.cast.framework.CastButtonFactory;
+import com.google.android.gms.cast.framework.CastContext;
+import com.google.android.gms.cast.framework.SessionManager;
+
 public class MainActivity extends AppCompatActivity {
+
+    private SessionManager sessionManager;
+    private final CastSessionListener castSessionListener = new CastSessionListener();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CastContext castContext = CastContext.getSharedInstance(this);
+        sessionManager = castContext.getSessionManager();
+
+        MediaRouteButton castButton = findViewById(R.id.media_route_button);
+        CastButtonFactory.setUpMediaRouteButton(getApplicationContext(), castButton);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sessionManager.addSessionManagerListener(castSessionListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sessionManager.removeSessionManagerListener(castSessionListener);
     }
 }
